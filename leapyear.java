1bc79941e1a13e575f301025c8c7316d2e4bf096
bhavaniprasadjolam <bhavaniprasadjolam1906@gmail.com>
2024-07-18 20:19:39 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,18 @@
+package varables;
+
+public class leapyear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//leap year or not
+		int year=2025;
+		if(year%4==0) {
+			System.out.println("is leap year ");
+		}
+		else {
+			System.out.println("non leap year ");
+		}
+
+	}
+
+}
